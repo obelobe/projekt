b92bd8bfd3dceb7e2f8f75c70d2523d39fbc1cbf
obelobe <31468928+obelobe@users.noreply.github.com>
2017-09-06 09:56:35 +0200

    Add files via upload

---
+++ send.php
@@ -0,0 +1,34 @@
+﻿<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Untitled Document</title>
+    </head>
+    
+    <body>
+    	<?php
+			$namn = $_POST['projektnamn'];
+			
+			echo $namn;
+			
+			$host = "localhost"; // Den server som kör MySQL
+			$user = "root"; // Användarnamn till MySQL
+			$pass = ""; // Lösenord till MySQL
+			$databas = "oskar"; // Databasens namn
+			
+			$conn = mysqli_connect($host, $user, $pass, $databas);
+			if(! $conn)
+			{
+				echo "Anslutningen misslyckades.";
+				exit; 
+			}
+			
+			$sql = "insert into förslag (namn) values ('$namn');";
+			mysqli_query($conn, $sql);
+			mysqli_close($conn);
+			
+			//header('Location: index.php');
+		?>
+        
+    </body>
+</html>
